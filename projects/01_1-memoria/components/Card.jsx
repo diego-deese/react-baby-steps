af24c8d4286a178428c7f1b413a7162643c05d3d
diego-deese <diego_dsgg@hotmail.com>
2023-07-03 23:26:25 -0600

    add styles and more functions

--- projects/01_1-memoria/components/Card.jsx
+++ projects/01_1-memoria/components/Card.jsx
@@ -1,11 +1,14 @@
-export const Card = ({ charName, imgName, isCovered }) => {
+export const Card = ({ charName, imgName, isCovered, updateCard, index}) => {
     const imgPath = `./img/${imgName}.jpg`
     const className = `card ${isCovered ? 'is-covered' : ''}`
+    charName = isCovered ? '?' : charName
 
-    if (isCovered) charName = '?'
+    const handleClick = () => {
+        updateCard(index)
+    }
 
     return (
-        <div className={className}>
+        <div onClick={handleClick} className={className}>
             {isCovered ? '' : <img src={imgPath} className="character-image"/>}
             {charName}
         </div>
--- projects/01_1-memoria/src/App.jsx
+++ projects/01_1-memoria/src/App.jsx
@@ -1,12 +1,64 @@
+import { useState } from 'react'
+
 import { Card } from '/components/Card.jsx'
 
 function App() {
+  const initialCards = [
+    { charName: 'Diego Sahid', imgName: 'Diego', isCovered: true, isMatched: false },
+    { charName: 'Benijugoso', imgName: 'Beniju', isCovered: true, isMatched: false },
+    { charName: 'MiduDev', imgName: 'Midu', isCovered: true, isMatched: false },
+    { charName: 'Diego Sahid', imgName: 'Diego', isCovered: true, isMatched: false },
+    { charName: 'MiduDev', imgName: 'Midu', isCovered: true, isMatched: false },
+    { charName: 'Benijugoso', imgName: 'Beniju', isCovered: true, isMatched: false }
+  ]
+
+  const [inGuessingMode, setInGuessingMode] = useState(false)
+  const [totalGuess, setTotalGuess] = useState(0)
+  const [cardInGuessing, setCardInGuessing] = useState(null)
+  const [cards, setCards] = useState(initialCards)
+  const [msj, setMsj] = useState('Escoge una carta')
+
+  const checkGuess = (index, compareName) => {
+    console.log('checkGuess')
+  }
+
+  const updateCard = (index) => {
+    const newCards = [...cards]
+
+    if (inGuessingMode) {
+      const hasGuessed = checkGuess(index, newCards[index].charName)
+    } else {
+      newCards[index].isCovered = !newCards[index].isCovered
+      setCards(newCards)
+      setCardInGuessing(newCards[index].charName)
+      const newGuessingMode = !inGuessingMode
+      setInGuessingMode(newGuessingMode)
+      console.log('inGuessingMode', inGuessingMode)
+    }
+  }
+
   return (
     <main className='board'>
       <h1>Memoria</h1>
+      <h2>Cartas Emparejadas: {totalGuess}</h2>
       <section className='cards'>
-        <Card charName='Diego Sahid' imgName='Diego' isCovered={false}></Card>
+        {
+          cards.map((card, index) => {
+            return (
+              <Card
+                key={index}
+                index={index}
+                updateCard={updateCard}
+                imgName={card.imgName}
+                charName={card.charName}
+                isCovered={card.isCovered}
+              >
+              </Card>
+            )
+          })
+        }
       </section>
+      <p className='info'>{msj}</p>
     </main>
   )
 }
